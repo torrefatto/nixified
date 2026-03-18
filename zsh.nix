8ad86081d1fdd4d67a99f73924f6023ace84f2e9
Leonardo Barcaroli <leonardo.barcaroli@mistral.ai>
2026-03-18 10:26:05 +0100

    Improve zsh config

--- zsh.nix
+++ zsh.nix
@@ -1,16 +1,21 @@
-{
+{ lib, ... }
+:{
   programs.zsh = {
     enable = true;
+    enableVteIntegration = true;
+    autosuggestion.enable = true;
 
     antidote = {
       enable = true;
       plugins = [
         "zsh-users/zsh-autosuggestions"
+        "zsh-users/zsh-completions"
         "zsh-users/zsh-syntax-highlighting"
       ];
     };
 
     shellAliases = {
+      ls = "ls --color=auto";
       ll = "ls -l";
       la = "ls -la";
       l = "ls -1";
@@ -21,14 +26,54 @@
       jjj = "jj-fzf";
     };
 
-    initContent = ''
-    function ,cd () {
-        [ "z''${_ZROOT}" != "z" ] && cd "''${_ZROOT}" && return 0
-        [ "z''${TMUX_BASE}" != "z" ] && cd "''${TMUX_BASE}" && return 0
-        cd ~
-    }
+    setOptions = [
+       "COMPLETE_ALIASES"
+    ];
 
-    bindkey -e
-    '';
+    initContent = let
+      zshFunctions = lib.mkOrder 500 ''
+        # Functions
+
+        function ,cd () {
+            [ "z''${_ZROOT}" != "z" ] && cd "''${_ZROOT}" && return 0
+            [ "z''${TMUX_BASE}" != "z" ] && cd "''${TMUX_BASE}" && return 0
+            cd ~
+        }
+
+        function __regen_or_source() {
+            local source="''${HOME}/.config/zsh_completions/''${1}"
+            local source_command="''${2}"
+
+            if [ ! -f ''${source} ] || find ''${source} -type f -mtime +1 | grep -q .; then
+                zsh -c "''${source_command}" > ''${source}
+            fi
+
+            . ''${source}
+        }
+      '';
+      zshCompletions = lib.mkOrder 1000 ''
+        # Completions and bindings
+
+        __regen_or_source _tailscale "/Applications/Tailscale.app/Contents/MacOS/Tailscale completion zsh"
+
+        # Function-y aliases
+
+        function tailscale() {
+            /Applications/Tailscale.app/Contents/MacOS/Tailscale ''${@}
+        }
+
+        # Completion paths
+
+        fpath+=("''${HOME}/.config/zsh_completions")
+
+        # Binds
+
+        bindkey -e
+      '';
+    in
+        lib.mkMerge [
+            zshFunctions
+            zshCompletions
+        ];
   };
 }
