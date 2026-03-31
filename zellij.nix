cb5851f2980e12a19521c0a87f6a4763f532feaf
Leonardo Barcaroli <leonardo.barcaroli@mistral.ai>
2026-03-31 11:46:05 +0200

    zellij: only tmux bindings

--- zellij.nix
+++ zellij.nix
@@ -5,6 +5,13 @@
     extraConfig = ''
       keybinds {
         unbind "Alt f"
+        normal clear-defaults=true {
+            bind "Ctrl g" { SwitchToMode "locked"; }
+            bind "Ctrl b" { SwitchToMode "tmux"; }
+        }
+        locked {
+            bind "Ctrl g" { SwitchToMode "Normal"; }
+        }
       }
     '';
   };
