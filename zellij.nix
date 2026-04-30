f4d1effaae9de501aea37dd7273a5e60696d9201
Leonardo Barcaroli <leonardo.barcaroli@mistral.ai>
2026-04-30 16:05:55 +0200

    Improve zellij config

--- zellij.nix
+++ zellij.nix
@@ -6,11 +6,11 @@
       keybinds {
         unbind "Alt f"
         normal clear-defaults=true {
-            bind "Ctrl g" { SwitchToMode "locked"; }
+            bind "Ctrl k" { SwitchToMode "locked"; }
             bind "Ctrl b" { SwitchToMode "tmux"; }
         }
         locked {
-            bind "Ctrl g" { SwitchToMode "Normal"; }
+            bind "Ctrl k" { SwitchToMode "Normal"; }
         }
       }
     '';
