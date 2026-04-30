585a4e02f2052fde3ee0a756170b56268db0a3f8
Leonardo Barcaroli <leonardo.barcaroli@mistral.ai>
2026-04-30 16:06:37 +0200

    Improve ssh config: fixed lenght socket names

--- ssh.nix
+++ ssh.nix
@@ -6,7 +6,11 @@
       "*" = {
         controlMaster = lib.mkForce "auto";
         controlPersist = lib.mkForce "1800";
+        controlPath = lib.mkForce "~/.ssh/sockets/%C";
         identityAgent = "/Users/leonardo.barcaroli/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+        setEnv = {
+          TERM = "xterm";
+        };
       };
     };
   };
