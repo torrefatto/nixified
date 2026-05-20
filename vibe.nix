3ca9dc69c7a1b107c8d33faf9c07c91dc8846d3e
Leonardo Barcaroli <leonardo.barcaroli@mistral.ai>
2026-05-20 14:40:51 +0200

    Add notion mcp to vibe config

--- vibe.nix
+++ vibe.nix
@@ -1,5 +1,9 @@
 {
   programs.mistral-vibe.settings = {
     enable_notifications = true;
+
+    mcp_servers = [
+      {name="notion"; transport="http"; url = "https://mcp.notion.com/mcp";}
+    ];
   };
 }
