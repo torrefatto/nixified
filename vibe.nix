3db90e1d6122205f20be120afc44c6bb352c90dc
Leonardo Barcaroli <leonardo.barcaroli@mistral.ai>
2026-07-01 11:18:44 +0200

    vibe: override default model

--- vibe.nix
+++ vibe.nix
@@ -1,6 +1,10 @@
+{ lib, ... }:
 {
   programs.mistral-vibe.settings = {
     enable_notifications = true;
+
+    #active_model = lib.mkForce "mistral-large-4";
+    active_model = "opus-4.8";
 
     mcp_servers = [
       {name="notion"; transport="http"; url = "https://mcp.notion.com/mcp"; disabled = false;}
